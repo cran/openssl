116b2fec554daf96f6950d34bf05407e683e8c60
Jeroen Ooms <jeroen@berkeley.edu>
2018-11-15 13:20:03 +0000

    version 1.1

--- R/askpass.R
+++ R/askpass.R
@@ -7,6 +7,35 @@
 #' @export
 #' @param prompt the string printed when prompting the user for input.
 askpass <- function(prompt = "Please enter your password: "){
-  FUN <- getOption("askpass", readline)
+  if(!interactive())
+    return(NULL)
+  FUN <- getOption("askpass", readline_silent)
   FUN(prompt)
 }
+
+readline_silent <- function(prompt){
+  if(is_unix() && isatty(stdin())){
+    if(system('stty -echo') == 0){
+      on.exit(system('stty echo'))
+    }
+  }
+  cat(prompt, "\n")
+  out <- base::readline("\U0001f511 ")
+  cat(" OK\n")
+  out
+}
+
+readline_bash <- function(prompt){
+  args <- sprintf('-s -p "%s" password && echo $password', prompt)
+  on.exit({system('stty echo'); cat("\n")})
+  system2('read', args, stdout = TRUE)
+}
+
+is_unix <- function(){
+  .Platform$OS.type == "unix"
+}
+
+is_macos <- function(){
+  identical(tolower(Sys.info()[['sysname']]), "darwin")
+}
+
